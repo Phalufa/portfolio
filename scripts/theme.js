c02b30855b9ec56806568ed11af9f2adba85a747
Phalufa <gchaimowicz@gmail.com>
2020-07-25 17:09:51 +0300

    Updates

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,11 +1,21 @@
-const checkbox = document.querySelector('input[name=theme]');
+const checkbox = document.querySelector('input[name=theme]')
+const mode = localStorage.getItem('mode')
 
 let switchTheme = () => {
-    checkbox.addEventListener('change', function() {
-        const de = document.documentElement;
-        this.checked ?
-            de.setAttribute('data-theme', 'dark') : de.setAttribute('data-theme', 'light')
-    })
+    const de = document.documentElement
+    de.setAttribute('data-theme', mode )
+
+    if (mode === 'dark')
+        checkbox.setAttribute('checked', true)
+    
+    checkbox.addEventListener('change', function () {
+
+        this.checked
+        ? de.setAttribute('data-theme', 'dark')
+        : de.setAttribute('data-theme', 'light')
+        
+        localStorage.setItem('mode', de.getAttribute('data-theme'))
+	})
 }
 
 export { switchTheme }
